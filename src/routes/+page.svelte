ca1f45bde656691813fbcad551edfa7b7666f96d
Mino2050 <116184204+Mino2050@users.noreply.github.com>
2023-02-01 07:34:19 -0800

    1 file modified, 1 added, 2 deleted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <script>
 	import { Avatar } from '@skeletonlabs/skeleton'
 	import ZeMadCoder from '$lib/images/ZeMadCoder.jpg'
-	import Mino from '$lib/images/Mino.png'
+	import Sui from '$lib/images/Sui.jpg'
 	import { toastStore } from '@skeletonlabs/skeleton'
 	function triggerToast() {
 		const t = {
@@ -46,8 +46,8 @@
 				</li>
 				<li>
 					<div class="flex items-center justify-center">
-						<Avatar class="mr-4" src={Mino} />
-						<span>Mino</span>
+						<Avatar class="mr-4" src={Sui} />
+						<span>Sui</span>
 					</div>
 				</li>
 			</ul>
